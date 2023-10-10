7cea2ff42ba43dd19be4f9e8e84d29e941097a0b
eLafo <javierlafora@gmail.com>
2023-10-10 12:24:50 +0200

    extracts api to its own module

---
+++ api/__init__.py
@@ -0,0 +1,77 @@
+import asyncio
+
+import langsmith
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from langsmith import Client
+client = Client()
+
+api = FastAPI()
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+# TODO: Update when async API is available
+async def _arun(func, *args, **kwargs):
+    return await asyncio.get_running_loop().run_in_executor(None, func, *args, **kwargs)
+
+
+async def aget_trace_url(run_id: str) -> str:
+    for i in range(5):
+        try:
+            await _arun(client.read_run, run_id)
+            break
+        except langsmith.utils.LangSmithError:
+            await asyncio.sleep(1**i)
+
+    if await _arun(client.run_is_shared, run_id):
+        return await _arun(client.read_run_shared_link, run_id)
+    return await _arun(client.share_run, run_id)
+
+@api.post("/feedback")
+async def send_feedback(request: Request):
+    data = await request.json()
+    run_id = data.get("run_id")
+    if run_id is None:
+        return {
+            "result": "No LangSmith run ID provided",
+            "code": 400,
+        }
+    key = data.get("key", "user_score")
+    vals = {**data, "key": key}
+    client.create_feedback(**vals)
+    return {"result": "posted feedback successfully", "code": 200}
+
+
+@api.patch("/feedback")
+async def update_feedback(request: Request):
+    data = await request.json()
+    feedback_id = data.get("feedback_id")
+    if feedback_id is None:
+        return {
+            "result": "No feedback ID provided",
+            "code": 400,
+        }
+    client.update_feedback(
+        feedback_id,
+        score=data.get("score"),
+        comment=data.get("comment"),
+    )
+    return {"result": "patched feedback successfully", "code": 200}
+
+@api.post("/get_trace")
+async def get_trace(request: Request):
+    data = await request.json()
+    run_id = data.get("run_id")
+    if run_id is None:
+        return {
+            "result": "No LangSmith run ID provided",
+            "code": 400,
+        }
+    return await aget_trace_url(run_id)
