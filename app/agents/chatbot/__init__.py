b877f8801f26d70ba71f6130aacc019b1a9df386
eLafo <javierlafora@gmail.com>
2023-10-15 13:19:25 +0200

    refactors chatbot

--- app/agents/chatbot/__init__.py
+++ app/agents/chatbot/__init__.py
@@ -1,39 +1,26 @@
 from langchain.agents import tool
-from langchain.memory import ConversationBufferMemory
+from operator import itemgetter
+from langchain.memory import ConversationBufferMemory, ChatMessageHistory
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda, RunnableMap, RunnableParallel
 from langchain import hub
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
+from langchain.schema import HumanMessage, SystemMessage
 
 from app.llm import llm
 from app.chains import research_chain
+from app.chains.research_chain.rephrase_question_chain import chain as rephrase_question_chain
 
-@tool(return_direct=True)
-def perform_research(question: str):
-    """Performs a research. Use this tool when you need to answer questions about topics"""
-    return research_chain.invoke(question)
-    
-tools = [
-    perform_research
-]
-
-
-prompt = hub.pull("hwchase17/react-chat")
-
-prompt = prompt.partial(
-    tools=render_text_description(tools),
-    tool_names=", ".join([t.name for t in tools]),
-)
-
-llm_with_stop = llm.bind(stop=["\nObservation"])
-
-agent = {
-    "input": lambda x: x["input"],
-    "agent_scratchpad": lambda x: format_log_to_str(x['intermediate_steps']),
-    "chat_history": lambda x: x["chat_history"]
-} | prompt | llm_with_stop | ReActSingleInputOutputParser()
-
-from langchain.agents import AgentExecutor
-
-memory = ConversationBufferMemory(memory_key="chat_history")
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, memory=memory, handle_parsing_errors=True)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+agent = RunnableMap(
+    question = RunnablePassthrough(),
+    chat_history = (RunnableLambda(memory.load_memory_variables) | itemgetter("chat_history"))
+) | RunnableMap(
+    output=(rephrase_question_chain | research_chain),
+    input=RunnableLambda(itemgetter("question"))
+) | RunnableParallel(
+    memory = RunnableLambda(lambda x: memory.save_context({"input": x["input"]}, {"output": x["output"]})),
+    output = itemgetter("output")
+) | RunnableLambda(lambda x: x["output"])
--- app/chains/research_chain/rephrase_question_chain.py
+++ app/chains/research_chain/rephrase_question_chain.py
@@ -1,24 +1,10 @@
 import os
-from typing import List
 from operator import itemgetter
 from langchain.prompts import load_prompt
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableBranch, RunnableLambda
-from langchain.schema import HumanMessage, AIMessage, ChatMessage
 
 from app.llm import llm
-
-def _serialize_history(chat_history: List[ChatMessage]):
-    print("serializing")
-    converted_chat_history = []
-    for message in chat_history:
-        if message.get("human") is not None:
-            converted_chat_history.append(
-                HumanMessage(content=message["human"]))
-        if message.get("ai") is not None:
-            converted_chat_history.append(AIMessage(content=message["ai"]))
-    print(f"serialized: {converted_chat_history}")
-    return converted_chat_history
 
 prompt = load_prompt(os.path.dirname(__file__) + "/rephrase_question_prompt.yaml")
 
@@ -34,5 +20,5 @@ branch_chain = RunnableBranch(
 
 chain = {
     "question": RunnableLambda(itemgetter("question")),
-    "chat_history": (RunnableLambda(itemgetter("chat_history")) | _serialize_history)
-} | branch_chain | StrOutputParser
+    "chat_history": (RunnableLambda(itemgetter("chat_history")))
+} | branch_chain | StrOutputParser()
