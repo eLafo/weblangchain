013540f3187cca6cca4e1a603f3d86a670adc607
eLafo <javierlafora@gmail.com>
2023-10-16 13:40:38 +0200

    refactor

--- app/chains/__init__.py
+++ app/chains/__init__.py
@@ -1,5 +1,6 @@
 from .researcher_chain import create_researcher_chain
-from .research_chain import chain as research_chain
+from .rephrase_question_chain import chain as rephrase_question
+from .research_chain import chain as research
 from ..llm import llm
 from ..retrievers import contextual_compression_retriever
 
--- app/chatbots/researcher_chatbot/__init__.py
+++ app/chatbots/researcher_chatbot/__init__.py
@@ -2,15 +2,15 @@ from operator import itemgetter
 from langchain.memory import ConversationBufferMemory
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda, RunnableMap, RunnableParallel
 
-from app.chains import research_chain
-from app.chains.research_chain.rephrase_question_chain import chain as rephrase_question_chain
+from app.chains import research
+from app.chains import rephrase_question
 
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 researcher_chatbot = RunnableMap(
     question = RunnablePassthrough(),
     chat_history = (RunnableLambda(memory.load_memory_variables) | itemgetter("chat_history"))
 ) | RunnableMap(
-    output=(rephrase_question_chain | research_chain),
+    output=(rephrase_question | research),
     input=RunnableLambda(itemgetter("question"))
 ) | RunnableParallel(
     memory = RunnableLambda(lambda x: memory.save_context({"input": x["input"]}, {"output": x["output"]})),
